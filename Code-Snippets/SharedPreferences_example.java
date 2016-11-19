03764e1904326a0b3b18a5c09eb00cc8993df68c
Berkay YILDIZ <berkay-yildiz@hotmail.com>
2016-11-19 04:35:23 +0200

    Create SharedPreferences_example.java

---
+++ Code-Snippets/SharedPreferences_example.java
@@ -0,0 +1,45 @@
+
+
+/******* Create SharedPreferences *******/
+ 
+    SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE); 
+    Editor editor = pref.edit();
+ 
+ 
+/**************** Storing data as KEY/VALUE pair *******************/
+ 
+    editor.putBoolean("key_name1", true);           // Saving boolean - true/false
+    editor.putInt("key_name2", "int value");        // Saving integer
+    editor.putFloat("key_name3", "float value");    // Saving float
+    editor.putLong("key_name4", "long value");      // Saving long
+    editor.putString("key_name5", "string value");  // Saving string
+     
+    // Save the changes in SharedPreferences
+    editor.commit(); // commit changes
+ 
+ 
+/**************** Get SharedPreferences data *******************/
+ 
+// If value for key not exist then return second param value - In this case null
+ 
+    pref.getBoolean("key_name1", null);         // getting boolean
+    pref.getInt("key_name2", null);             // getting Integer
+    pref.getFloat("key_name3", null);           // getting Float
+    pref.getLong("key_name4", null);            // getting Long
+    pref.getString("key_name5", null);          // getting String
+ 
+ 
+ 
+ 
+/************ Deleting Key value from SharedPreferences *****************/
+ 
+    editor.remove("key_name3"); // will delete key key_name3
+    editor.remove("key_name4"); // will delete key key_name4
+     
+    // Save the changes in SharedPreferences
+    editor.commit(); // commit changes
+    
+/************ Clear all data from SharedPreferences *****************/  
+ 
+     editor.clear();
+     editor.commit(); // commit changes
